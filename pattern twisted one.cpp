df6981b0f40769b82a8fc111a81115d404a311ef
siri-1208 <146007219+siri-1208@users.noreply.github.com>
2023-09-25 21:56:29 +0530

    Add files via upload

---
+++ pattern twisted one.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    int n;
+    cin>>n;
+    int i;
+    while(i<=n){
+    for(int j=1;j<=i;j++){
+    char ch;
+    ch='A'+n-i+j-1;
+    cout<<ch<<" ";
+    }
+    cout<<endl;
+    i++;
+    }
+
+    return 0;
+}
